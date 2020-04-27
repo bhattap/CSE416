7a073bc5a23632cbae942f63766ea0a34a04231b
Puja Bhattacharjee <stevieisme3290@gmail.com>
2020-04-27 03:25:03 -0400

    Deleted everything other than src/main/webapp folder (where the jsp and css files are)

--- levelBuilder/src/main/java/levelBuilder/com/repositories/ConfirmationTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package levelBuilder.com.repositories;
-
-import levelBuilder.com.entities.ConfirmationTokenEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationTokenEntity, Integer> {
-    ConfirmationTokenEntity findByConfirmationToken(String confirmationToken);
-}
